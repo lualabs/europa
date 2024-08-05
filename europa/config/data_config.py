74c23189288919e756551b921bf0c8012cf2e27d
Rafael <rafaelmcelente@gmail.com>
2024-08-05 12:37:48 +0200

    fix(config): parsing hf token if he isnt passed on the config

--- europa/config/data_config.py
+++ europa/config/data_config.py
@@ -1,6 +1,7 @@
 import os
 from typing import Optional, List, Dict, Union, Any
-from pydantic import BaseModel, ConfigDict, field_validator
+from typing_extensions import Self
+from pydantic import BaseModel, field_validator, model_validator
 import yaml
 from transformers import AutoProcessor
 from .utils import load_obj
@@ -21,7 +22,7 @@ class DataConfig(BaseModel):
     train_collate_fn: Optional[str] = None
     eval_collate_fn: Optional[str] = None
     processor: str = "auto"
-    hf_token: str = None
+    hf_token: Optional[str] = None
     wandb_project: Optional[str] = None
 
     @staticmethod
@@ -47,64 +48,16 @@ class DataConfig(BaseModel):
             raise FileNotFoundError(f"Test data directory {v} not found")
         return v
 
-    @field_validator("hf_token", mode="before")
-    @classmethod
-    def parse_hf_token(cls, v):
-        if v is not None:
-            return v
-        token = os.getenv("HF_TOKEN")
-        if token is None:
+    @model_validator(mode="after")
+    def parse_hf_token(self) -> Self:
+        if self.hf_token is not None:
+            return self
+        self.hf_token = os.getenv("HF_TOKEN")
+        if self.hf_token is None:
             raise ValueError("Variable hf_token not passed and HF_TOKEN not found in environment variables.")
-
+        return self
+    
     def load_processor(self, args: Optional[Dict[str, Any]] = None):
         if self.processor == "auto":
             return AutoProcessor.from_pretrained(self.repo_id, token=self.hf_token)
         return load_obj(self.processor, **args)
-
-
-# class DocClassifierConfig(BaseModel):
-#     model_config  = ConfigDict(protected_namespaces=())
-#     num_classes: int
-#     from_pretrained: bool
-#     model_name: str = "mobilenetv3_small_100"
-
-#     @staticmethod
-#     def from_yaml(config_file):
-#         if not os.path.exists(config_file):
-#                 raise FileNotFoundError(f"No path to config file found: {config_file}")
-#         with open(config_file) as f:
-#             config = yaml.load(f, Loader=yaml.FullLoader)
-#         return DocClassifierConfig(**config)
-
-#     def load_optimizer(self, params):
-#         """
-#         Loads the optimizer from the configuration.
-
-#         Args:
-#             params: The model parameters. This is equivelent to self.parameters().
-#         """
-#         return self.optimizer.load(params)
-
-#     def load_scheduler(self, optimizer):
-#         """
-#         Loads the scheduler from the configuration.
-
-#         Args:
-#             optimizer: The optimizer to be used with the scheduler.
-#         """
-#         return self.scheduler.load(optimizer)
-    
-#     def load_metric(self, num_classes):
-#         """
-#         Loads the metric from the configuration
-
-#         Args:
-#             num_classes: The number of classes in the dataset
-#         """
-#         return self.metric.load(num_classes,)
-    
-#     def load_loss_fn(self):
-#         """
-#         Loads the loss function from the configuration
-#         """
-#         return self.loss_fn.load()
